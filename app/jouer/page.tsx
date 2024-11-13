8d5d46247c11abc0f4aad82d265d0c4972124f4d
jplumail <jeanplumail76@gmail.com>
2024-11-13 20:09:02 +0100

    feat: add audio feedback for correct answers in game

--- app/jouer/page.tsx
+++ app/jouer/page.tsx
@@ -8,7 +8,6 @@ import DialogBox from './dialogueBox';
 import { useEffect, useRef, useState } from 'react';
 import { Message, StudioQuizEvent, Question, DateMilliseconds, Answer, State, GameStatus, Scores, Player } from '@/shared/types';
 
-
 export default function Play() {
     const [pseudo, setPseudo] = useState<string>('');
     const [messages, setMessages] = useState<Message[]>([]);
@@ -19,6 +18,7 @@ export default function Play() {
     const [answer, setAnswer] = useState<Answer | null>(null);
     const [questionStartDate, setQuestionStartDate] = useState<DateMilliseconds | null>(null);
     const [questionEndDate, setQuestionEndDate] = useState<DateMilliseconds | null>(null);
+    const correctAnswerAudio = useRef<HTMLAudioElement | null>(null);
 
     const [state, setState] = useState<State>("WAITING");
     const [gameStatus, setGameStatus] = useState<GameStatus | null>(null);
@@ -65,6 +65,9 @@ export default function Play() {
                 case 'correctAnswer':
                     setMessages(prevMessages => [...prevMessages, message]);
                     setHasAnswered(prevHasAnswered => new Map([...prevHasAnswered, [message.payload.player, true]]));
+                    if (correctAnswerAudio.current) {
+                        correctAnswerAudio.current.play();
+                    }
                     break;
                 case 'endQuestion':
                     setMessages(prevMessages => [...prevMessages, message]);
@@ -107,6 +110,7 @@ export default function Play() {
 
     return (
         <div className={styles.container}>
+            <audio ref={correctAnswerAudio} src="/correct-answer.mp3" />
             {(gameStatus == "QUESTION") && (questionStartDate && questionEndDate) && <div style={{ position: "absolute", margin: "0.5rem", zIndex: 1 }}><Clock startDate={questionStartDate} endDate={questionEndDate} /></div>}
             {sentence && <div style={{ position: "absolute", right: "1rem", top: "2rem", zIndex: 3 }}><DialogBox sentence={sentence} /></div>}
             <div className={styles.column} style={{ backgroundColor: "hsl(285.77deg 96.04% 19.8%)", display: 'grid', justifyItems: "center" }}>
