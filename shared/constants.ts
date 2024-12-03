f8c41a977627628e69fbea7c7da79d5e47dfd784
jplumail <jeanplumail76@gmail.com>
2024-12-03 23:19:30 +0100

    fix gameServerHostname in prod

--- shared/constants.ts
+++ shared/constants.ts
@@ -2,6 +2,6 @@ export const production = process.env.NODE_ENV === 'production';
 export const port = parseInt(process.env.PORT || "3000", 10);
 export const gameServerPort = parseInt(process.env.PORT || "8080", 10);
 export const hostname = process.env.HOST || "localhost";
-export const gameServerHostname = production ? `studioquiz-gameserver-${process.env.VERCEL_GIT_COMMIT_REF}-577380683277.europe-west9.run.app` : "localhost";
+export const gameServerHostname = production ? `studioquiz-gameserver-${process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_REF}-577380683277.europe-west9.run.app` : "localhost";
 export const gameServerUrl = production ? `https://${gameServerHostname}` : `http://${gameServerHostname}:${gameServerPort}`;
 export const mainServerUrl = production ? process.env.VERCEL_URL : `http://${hostname}:${port}`;
