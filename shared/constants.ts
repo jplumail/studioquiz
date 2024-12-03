b740334a22cfdf11b45f17bfa5da912b40531c20
jplumail <jeanplumail76@gmail.com>
2024-12-04 00:03:38 +0100

    fix: clean env vars

--- shared/constants.ts
+++ shared/constants.ts
@@ -3,7 +3,7 @@ export const production = process.env.NODE_ENV === 'production';
 export const mainServerPort = parseInt(process.env.PORT || "3000", 10);
 export const gameServerPort = parseInt(process.env.PORT || "8080", 10);
 
-export const mainServerHostname = production ? (process.env.NEXT_PUBLIC_VERCEL_URL || process.env.VERCEL_URL) : "localhost";
+export const mainServerHostname = production ? `studioquiz-git-${process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_REF || process.env.BRANCH_NAME}-jeans-projects-0b2ad0ac.vercel.app` : "localhost";
 export const gameServerHostname = production ? `studioquiz-gameserver-${process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_REF}-577380683277.europe-west9.run.app` : "localhost";
 
 export const mainServerUrl = production ? `https://${mainServerHostname}` : `http://${mainServerHostname}:${mainServerPort}`;
