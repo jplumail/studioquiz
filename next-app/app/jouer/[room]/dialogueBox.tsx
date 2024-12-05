d833449601ed45ddb6d1f8622eae7f7baac2d540
jplumail <jeanplumail76@gmail.com>
2024-12-05 20:25:57 +0100

    style: dialogueBox plus lisible

--- next-app/app/jouer/[room]/dialogueBox.tsx
+++ next-app/app/jouer/[room]/dialogueBox.tsx
@@ -1,7 +1,9 @@
 export default function Box({ sentence }: { sentence: string }) {
     return <div style={{
-        backgroundColor: "hsl(31.35deg 73.55% 52.55%)",
+        backgroundColor: "white",
         padding: "0.5rem",
         borderRadius: "0.5rem",
+        color: "hsl(31.35deg 73.55% 52.55%)",
+        fontWeight: "700",
     }}>{sentence}</div>
 }
