9a52c956dcb7707efc7e4086ee4509a9c47400ac
jplumail <jeanplumail76@gmail.com>
2024-12-07 18:44:29 +0100

    fix: ajoute des dimensions à l'image du présentateur

--- next-app/app/jouer/[room]/game.tsx
+++ next-app/app/jouer/[room]/game.tsx
@@ -129,7 +129,7 @@ export default function Game({ room, pseudo }: { room: types.RoomId, pseudo: typ
                 {(state == types.State.QUESTION) && (questionStartDate && questionEndDate) && <div style={{ position: "absolute", margin: "0.5rem", zIndex: 1 }}><Clock startDate={questionStartDate} endDate={questionEndDate} /></div>}
                 {sentence && <div style={{ position: "absolute", right: "1rem", top: "2rem", zIndex: 3 }}><DialogBox sentence={sentence} /></div>}
                 <div className={styles.column} style={{ backgroundColor: "hsl(285.77deg 96.04% 19.8%)", display: 'grid', justifyItems: "center" }}>
-                    <div style={{ position: "relative", left: "40px", zIndex: 0 }}><Image src='/presentateur.jpeg' alt='Présentateur' id={styles.presentateur}/></div>
+                    <div style={{ position: "relative", left: "40px", zIndex: 0 }}><Image src='/presentateur.jpeg' width={219} height={230} alt='Présentateur' id={styles.presentateur}/></div>
                     <Scoreboard scores={scores} hasAnswered={hasAnswered} />
                 </div>
                 <div className={styles.column}>
