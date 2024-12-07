f0520faaeaa43cd5a7dabb21d265b5955e360472
jplumail <jeanplumail76@gmail.com>
2024-12-07 19:37:28 +0100

    feat: dialogueBox meilleur style sur mobile

--- next-app/app/jouer/[room]/dialogueBox.tsx
+++ next-app/app/jouer/[room]/dialogueBox.tsx
@@ -1,9 +1,5 @@
+import styles from './page.module.css'
+
 export default function Box({ sentence }: { sentence: string }) {
-    return <div style={{
-        backgroundColor: "white",
-        padding: "0.5rem",
-        borderRadius: "0.5rem",
-        color: "hsl(31.35deg 73.55% 52.55%)",
-        fontWeight: "700",
-    }}>{sentence}</div>
+    return <div id={styles.dialogueBox}>{sentence}</div>
 }
--- next-app/app/jouer/[room]/game.tsx
+++ next-app/app/jouer/[room]/game.tsx
@@ -127,9 +127,9 @@ export default function Game({ room, pseudo }: { room: types.RoomId, pseudo: typ
             <div className={styles.container}>
                 <audio ref={correctAnswerAudio} src="/correct-answer.mp3" />
                 {(state == types.State.QUESTION) && (questionStartDate && questionEndDate) && <div style={{ position: "absolute", margin: "0.5rem", zIndex: 1 }}><Clock startDate={questionStartDate} endDate={questionEndDate} /></div>}
-                {sentence && <div style={{ position: "absolute", right: "1rem", top: "2rem", zIndex: 3 }}><DialogBox sentence={sentence} /></div>}
                 <div className={styles.column} style={{ backgroundColor: "hsl(285.77deg 96.04% 19.8%)", display: 'grid', justifyItems: "center" }}>
                     <div style={{ position: "relative", left: "40px", zIndex: 0 }}><Image src='/presentateur.jpeg' width={219} height={230} alt='Présentateur' id={styles.presentateur}/></div>
+                    {sentence && <DialogBox sentence={sentence} />}
                     <Scoreboard scores={scores} hasAnswered={hasAnswered} />
                 </div>
                 <div className={styles.column}>
